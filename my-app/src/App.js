a37c32bad66ddb079656e0caea9e40339a0bda64
xiangyi0818 <34527543+xiangyi0818@users.noreply.github.com>
2021-03-18 18:41:03 -0700

    change css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,12 +16,14 @@ class App extends React.Component {
     console.log(this.props)
     return(
       <div className="Roll">
+      <div className="button">
       <button 
       style={{width:"60px", height:"60px",border:"1px solid grey"}}
       onClick={()=> this.props.dispatch({type:"ROLL_DICE"})}> <h3>roll dice</h3></button> 
       <button 
       style={{width:"60px", height:"60px",border:"1px solid grey"}}
       onClick={()=> this.props.dispatch({type:"CLEAR_DICE"})}> <h3>clear dice</h3></button>
+      </div>
       <div>
         <h3>Sum: {this.props.diceList.reduce((a,b)=> a+b, 0)}</h3>
       </div>
